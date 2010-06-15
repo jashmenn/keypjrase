7480fdd4f5f4deb0e61aa01e1fd726e2c4a1a278
Nate Murray <nate@natemurray.com>
2010-06-15 08:52:04 -0700

    working on testing the classifier

--- src/clj/keypjrase/test.clj
+++ src/clj/keypjrase/test.clj
@@ -2,15 +2,33 @@
   (:require clojure.contrib.str-utils)
   (:require [clojure.contrib [map-utils :as m]])
   (:require [keypjrase [parser :as parser] 
-              [document :as document] [instance :as instance]
+              [document :as d] [instance :as instance]
               [classifier :as classifier]] :reload)
   (:use [keypjrase.util] :reload)
   (:use clojure.contrib.command-line)
   (:gen-class))
 
-(defn test-document [document stats classifier]
-  (let [instances  (instance/create-instances-w-docs [document] stats)]
+(defn classify-instance [instance classifier]
+  (let [ds (classifier/build-dataset-from-instances [instance])]
+    ; here, how do we filter the instance?
+    ; do we add it to the classifier's existing dataset? can we convert it?
+    ; then we need to call classify instance on it. note that it must belong to an dataset
+    ; see 
   )
+
+(defn predict-tags 
+  "given list of instance-structs, returns a list of predicted tags and their
+  probabilities"
+  [instances classifier]
+  (reduce 
+    (fn [acc instance] (cons (classify-instance instance classifier) acc))
+    '() instances))
+
+(defn test-a-document [document stats classifier]
+  (let [instances (instance/create-instances-w-docs [document] stats)
+        predicted-tags (predict-tags instances classifier)]
+  instances))
+(test-a-document d/test-document test-stats test-classifier)
 
 (defn perform-test [documents collection-stats classifier]
   (let [test-stats {}
@@ -18,8 +36,8 @@
     (reduce
       (fn [acc document]
       (m/deep-merge-with + acc 
-                         (test-document document collection-stats classifier)))
-      test-stats documents))
+                         (test-a-document document collection-stats classifier)))
+      test-stats documents)))
 
 (defn -test 
   "two things, extraction and test"
@@ -36,3 +54,16 @@
    ]
     (perform-test documents stats classifier)
   ))
+
+(comment 
+
+  (perform-test d/test-documents test-stats test-classifier)
+
+  (test-a-document d/test-document test-stats test-classifier)
+
+  (def test-instance-struct {:token "dog", :class true, :features {:distance 1/10, :tfidf 0.5991464547107982, :pos nil}})
+
+  (def test-stats (read-data-structure "tmp/runs/stats.clj"))
+  (def test-classifier (classifier/restore "tmp/runs/classifier"))
+
+  )
